2ee9f14973f5e00fdb5fb77a5d84d5b511b9f989
rlabbiz <redalabbiz@gmail.com>
2024-11-20 14:27:34 +0100

    finish the user history i think

--- FrontEnd/components/profile.js
+++ FrontEnd/components/profile.js
@@ -3,18 +3,16 @@ import {
     fetchProfile, 
     globalState,
     fetchUsers,
+    fetchGamesResults,
 } from '../scripts/fetchData.js';
     
 export async function profileComponent() {
-    if (globalState.user === null) 
-        await fetchProfile();
-    
-    if (globalState.users === null) 
-        await fetchUsers();
+    await fetchProfile();
+    await fetchUsers();
+    await fetchGamesResults();
 
-    if (globalState.user === null || globalState.users === null) {
+    if (globalState.user === null || globalState.users === null || globalState.gamesResults === null)
         return (`cant fetch user data`)
-    }
 
     // get username parameter from url to fetch user data
     const urlParams = new URLSearchParams(window.location.search);
@@ -41,14 +39,14 @@ export async function profileComponent() {
 function profileContent(user) {
     return (`
         <div id="profile">
-            <div class="profile-header" w-tid="11" style="/* transform: rotateY(-0.54deg) rotateX(-15.4deg); */">
-                <div class="profile-pic-container" w-tid="12">
-                    <img src="${user.avatar}" alt="${user.username}" class="profile-pic" w-tid="13" data-image_id="0" alt-rewritten="Neon-lit ping pong match captured in vibrant digital photograph." style="box-shadow: rgba(61, 189, 167, 0.5) 0px 0px 20px;">
+            <div class="profile-header">
+                <div class="profile-pic-container">
+                    <img src="${user.avatar}" alt="${user.username}" class="profile-pic" style="box-shadow: rgba(61, 189, 167, 0.5) 0px 0px 20px;">
                 </div>
-                <div class="profile-info" w-tid="15">
-                    <h1 w-tid="16">${user.first_name} ${user.last_name}</h1>
-                    <p w-tid="17">@${user.username}</p>
-                    <p w-tid="18">Level ${user.game_stats[0].level}</p>
+                <div class="profile-info">
+                    <h1>${user.first_name} ${user.last_name}</h1>
+                    <p>@${user.username}</p>
+                    <p>Level ${user.game_stats[0].level}</p>
                     ${profileButtons(user)}
                 </div>
             </div>
@@ -72,36 +70,36 @@ function profileContent(user) {
                 </div>
             </div>
 
-            ${getLastMatches()}
-            <div class="achievements" w-tid="65">
-                <h2 w-tid="66">Achievements</h2>
-                <div class="achievement-grid" w-tid="67">
-                    <div class="achievement-item" w-tid="68">
-                        <div class="achievement-icon" w-tid="69">🏆</div>
-                        <div class="achievement-info" w-tid="70">
-                            <h3 w-tid="71">Tournament Champion</h3>
-                            <p w-tid="72">Win a major PingPongMasters tournament</p>
+            ${getLastMatches(user)}
+            <div class="achievements">
+                <h2>Achievements</h2>
+                <div class="achievement-grid">
+                    <div class="achievement-item">
+                        <div class="achievement-icon">🏆</div>
+                        <div class="achievement-info">
+                            <h3>Tournament Champion</h3>
+                            <p>Win a major PingPongMasters tournament</p>
                         </div>
                     </div>
-                    <div class="achievement-item" w-tid="73">
-                        <div class="achievement-icon" w-tid="74">🔥</div>
-                        <div class="achievement-info" w-tid="75">
-                            <h3 w-tid="76">On Fire</h3>
-                            <p w-tid="77">Win 10 matches in a row</p>
+                    <div class="achievement-item">
+                        <div class="achievement-icon">🔥</div>
+                        <div class="achievement-info">
+                            <h3>On Fire</h3>
+                            <p>Win 10 matches in a row</p>
                         </div>
                     </div>
-                    <div class="achievement-item" w-tid="78">
-                        <div class="achievement-icon" w-tid="79">🎯</div>
-                        <div class="achievement-info" w-tid="80">
-                            <h3 w-tid="81">Precision Master</h3>
-                            <p w-tid="82">Achieve 95% serve accuracy in 50 matches</p>
+                    <div class="achievement-item">
+                        <div class="achievement-icon">🎯</div>
+                        <div class="achievement-info">
+                            <h3>Precision Master</h3>
+                            <p>Achieve 95% serve accuracy in 50 matches</p>
                         </div>
                     </div>
-                    <div class="achievement-item" w-tid="83">
-                        <div class="achievement-icon" w-tid="84">⚡</div>
-                        <div class="achievement-info" w-tid="85">
-                            <h3 w-tid="86">Speed Demon</h3>
-                            <p w-tid="87">Win a match in under 5 minutes</p>
+                    <div class="achievement-item">
+                        <div class="achievement-icon">⚡</div>
+                        <div class="achievement-info">
+                            <h3>Speed Demon</h3>
+                            <p>Win a match in under 5 minutes</p>
                         </div>
                     </div>
                 </div>
@@ -110,39 +108,53 @@ function profileContent(user) {
     `)
 }
 
-export function getLastMatches() {
+export function getLastMatches(user) {
+    const lastMatches = globalState.gamesResults.filter(game => {
+        return (game.player_1 === user.username || game.player_2 === user.username)
+    })
     return (`
         <div class="match-history" w-tid="42">
             <h2 w-tid="43" class="">Recent Matches</h2>
             <div class="match-list" w-tid="44">
-                <div class="match-item" w-tid="45">
-                    <span w-tid="46">vs. John Doe</span>
-                    <span w-tid="47">Score: 21-18</span>
-                    <span class="match-result win" w-tid="48">Win</span>
-                </div>
-                <div class="match-item" w-tid="49">
-                    <span w-tid="50">vs. Emma Smith</span>
-                    <span w-tid="51">Score: 19-21</span>
-                    <span class="match-result loss" w-tid="52">Loss</span>
-                </div>
-                <div class="match-item" w-tid="53">
-                    <span w-tid="54">vs. Mike Johnson</span>
-                    <span w-tid="55">Score: 21-15</span>
-                    <span class="match-result win" w-tid="56">Win</span>
-                </div>
-                <div class="match-item" w-tid="57">
-                    <span w-tid="58">vs. Lisa Brown</span>
-                    <span w-tid="59">Score: 21-12</span>
-                    <span class="match-result win" w-tid="60">Win</span>
-                </div>
-                <div class="match-item" w-tid="61">
-                    <span w-tid="62">vs. Tom Wilson</span>
-                    <span w-tid="63">Score: 18-21</span>
-                    <span class="match-result loss" w-tid="64">Loss</span>
-                </div>
+                ${getMatches(user, lastMatches)}
             </div>
         </div>
     `)
+}
+
+function getMatches(user, games){
+    const innerHtml = games.map(game => {
+        let score = '';
+        let vs = '';
+        let isWin = false;
+
+        if (game.player_1 === user.username && game.score_1 > game.score_2) 
+            isWin = true;
+        else if (game.player_2 === user.username && game.score_2 > game.score_1) 
+            isWin = true;
+
+        if (game.player_1 === user.username) {
+            score = `${game.score_1} - ${game.score_2}`;
+            vs = `${game.player_1} vs. ${game.player_2}`;
+        } else {
+            score = `${game.score_2} - ${game.score_1}`;
+            vs = `${game.player_2} vs. ${game.player_1}`;
+        }
+    
+        return (`
+            <div class="match-item">
+                <span>${vs}</span>
+                <span>Score: ${score}</span>
+                <span class="match-result ${isWin ? 'win': 'loss'}">${isWin ? 'Win': 'Lost'}</span>
+            </div>
+        `)
+    })
+    if (innerHtml.length === 0)
+        return (`<p>No matches found</p>`)
+    else if (innerHtml.length > 5)
+        return innerHtml.slice(0, 5).join('\n');
+    else
+        return innerHtml.join('\n');
 }
 
 function profileButtons(user) {
